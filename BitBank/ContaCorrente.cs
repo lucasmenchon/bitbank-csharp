7661ae7315adf9053b3aaa33a5ea3d4083fdfb9a
lucasmenchon <it.lucas@outlook.com>
2022-04-03 13:40:24 -0300

    adicionado novos metodos, saque, saldo atual, tudo com calculos e funcao

--- BitBank/ContaCorrente.cs
+++ BitBank/ContaCorrente.cs
@@ -1,10 +1,28 @@
 ﻿
 
 
+using System;
+
 public class ContaCorrente
 {
     public string titular;
     public int agencia;
     public int numero;
-    public double saldo;
+    public double saldo = 1000;
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+
 }
+
--- BitBank/Program.cs
+++ BitBank/Program.cs
@@ -11,19 +11,32 @@ namespace BitBank
             int numConta = int.Parse( Console.ReadLine());
             int saldo = int.Parse(Console.ReadLine());*/
 
-            Titulares[] Titulares = new Titulares[999];
+            /*Titulares Titulares = new Titulares();
+            string atitular = Titulares.Titular();
+            string btitular = Titulares.Titular();*/
 
             
-           /* ContaCorrente contaGabriel = new ContaCorrente();
-            contaGabriel.titular = Console.ReadLine();
-            contaGabriel.agencia = int.Parse(Console.ReadLine());
-            contaGabriel.numero = int.Parse(Console.ReadLine());
-            contaGabriel.saldo = double.Parse(Console.ReadLine());
+            ContaCorrente conta = new ContaCorrente();
+            Console.WriteLine("Titular: ");
+            conta.titular = Console.ReadLine();
+            Console.WriteLine("Agência: ");
+            conta.agencia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Número da Conta: ");
+            conta.numero = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Titular: " +contaGabriel.titular);
-            Console.WriteLine("Agência: " + contaGabriel.agencia);
+            Console.WriteLine("Titular: " +conta.titular);
+            Console.WriteLine("Agência: " + conta.agencia);
+            Console.WriteLine("Número da Conta: " + conta.numero);
+            Console.WriteLine("Saldo: " + conta.saldo);
 
-            Console.ReadKey();*/
+            Console.WriteLine("Insira valor para saque: ");
+            conta.Sacar(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Sacando.. ");
+            Console.WriteLine("Saldo Atual: ");
+            Console.WriteLine(conta.saldo);
+            Console.WriteLine("Obrigado. Volte Sempre!");
+
+            Console.ReadKey();
             
 
         }
