5225a3e1332031cb80dfd0b293b8ce073d499e5a
lucasmenchon <it.lucas@outlook.com>
2022-04-03 00:24:13 -0300

    Adicionar arquivos de projeto.

---
+++ BitBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BitBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* string nomeTitular = Console.ReadLine();
+            int numAgencia = int.Parse(Console.ReadLine());
+            int numConta = int.Parse( Console.ReadLine());
+            int saldo = int.Parse(Console.ReadLine());*/
+
+            Titulares Titulares = new Titulares();
+            string xtitular = Titulares.Titular();
+            string ztitular = Titulares.Titular();
+
+            
+
+        }
+    }
+}
---
+++ BitBank/Titulares.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitBank
+{
+    class Titulares
+    {
+        public string Titular()
+
+        {
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite seu nome: ");
+
+            string nomeTitular = Console.ReadLine();
+
+            Console.WriteLine("Digite o número de sua agência: ");
+
+            double numAgencia = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o número de sua conta: ");
+
+            double numConta = double.Parse(Console.ReadLine());
+
+            /*Console.WriteLine("");
+
+            double saldo = double.Parse(Console.ReadLine());*/
+
+            return nomeTitular + numAgencia + numConta;
+        }// Titular
+
+    }
+}
